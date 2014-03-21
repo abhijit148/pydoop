5d05652c8ad2a46bc16fb465925e12dd334cbf12
Abhijit Agarwal <abhijit.148@gmail.com>
2014-03-21 19:00:54 +0530

    Mapper and Reducer scripts written

--- mapper.py
+++ mapper.py
@@ -1,1 +1,19 @@
-#!Python Mapper
+#!/usr/bin/env python
+#Python Hadoop Mapper
+
+import sys
+
+#input comes from STDIN (standard input)
+for line in sys.stdin:
+	line=line.strip()
+	words=line.split()
+	
+	#following section will output each word with a count of 1
+	#if a word appears twice, it will be output twice with a count 1
+	#adding the counts is left upto the reducer
+	for word in words:
+		#write the results to STDOUT
+		#this output will be input to reducer
+		
+		print '%s\t%s' % (word,1) 
+
--- reducer.py
+++ reducer.py
@@ -1,1 +1,38 @@
-#!Python Reducer
+#!usr/bin/env python
+
+from operator import itemgetter
+import sys
+
+current_word=None
+current_count=0
+word=None
+
+#input from STDIN
+for line in sys.stdin:
+	line=line.strip()
+	#parse the output of mapper.py i.e. input to reducer.py
+	word,count=line.split('\t',1)
+	
+	#covert count (string) to int
+	try:
+		count=int(count)
+	except ValueError:
+		#Count was not a number. Ignore.
+		continue
+		
+	#this IF-switch only works because Hadoop sorts map output
+	#by key (here:word_ before it is passed to the reducer
+	
+	if current_word==word:
+		current_count+=count
+	else:
+		if current_word:
+			#write to STDOUT
+			print '%s\t%s' % (current_word,current_count)
+		
+		current_count=count
+		current_word=word
+	
+#For last word (if needed)
+if current_word==word:
+	print '%s\t%s' % (current_word,current_count)	
